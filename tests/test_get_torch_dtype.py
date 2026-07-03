703923577e9a8250337222e380f1572ae012c47e
Ethan Lu <luethan2025@outlook.com>
2026-07-02 21:54:44 -0700

    implemented a check for bf16

--- tests/test_get_torch_dtype.py
+++ tests/test_get_torch_dtype.py
@@ -5,10 +5,19 @@ import torchutils
 
 @pytest.mark.parametrize("precision", ["fp16", "bf16", "no"])
 def test_get_torch_dtype(precision: str) -> None:
-    expected = {
+    if (
+        (precision == "bf16")
+        and not (
+            torch.cuda.is_available()
+            and torch.cuda.is_bf16_supported()
+        )
+    ):
+        pytest.skip("bf16 precision was requested but not available on this hardware.")
+    torch_dtypes = {
         "fp16": torch.float16,
         "bf16": torch.bfloat16,
         "no": torch.float32,
-    }[precision]
+    }
+    expected = torch_dtypes[precision]
 
     assert torchutils.get_torch_dtype(precision) == expected
--- torchutils/utils.py
+++ torchutils/utils.py
@@ -16,6 +16,14 @@ def set_seed(seed: int) -> None:
     torch.backends.cudnn.benchmark = False
 
 def get_torch_dtype(precision: str | torch.dtype) -> torch.dtype:
+    if (
+        (precision == "bf16" or precision == torch.bfloat16)
+        and not (
+            torch.cuda.is_available()
+            and torch.cuda.is_bf16_supported()
+        )
+    ):
+        raise RuntimeError("bf16 precision was requested but not available on this hardware. Please use fp32 or fp16 precision instead.")
     if isinstance(precision, torch.dtype):
         return precision
     if precision == "fp16":
